60f31e16f09dfba9c41c5621d31bb48088ff7363
AKHILA RAJAN <EXTERN.RAJAN_AKHILA@ALLIANZ.DE>
2018-03-23 10:58:09 +0530

    new changes

--- planview/src/main/java/com/spring/planview/controller/RegistrationController.java
+++ planview/src/main/java/com/spring/planview/controller/RegistrationController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,12 +13,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class RegistrationController {
 	
-	@RequestMapping(value="/" , method=RequestMethod.GET,produces={MediaType.APPLICATION_JSON_VALUE})
-	public @ResponseBody Map<String,String> getmessage()
+	@RequestMapping(value="/" , method=RequestMethod.POST,consumes={MediaType.APPLICATION_JSON_VALUE},produces={MediaType.APPLICATION_JSON_VALUE})
+	public @ResponseBody Person getmessage(@RequestBody Person p)
 	{
-		Map<String,String> map = new HashMap<String,String>();
-		map.put("id", "123");
-		map.put("name", "Akhila");
-		return map;
+		p.setName("abijith");
+		return p;
 	}
 }
